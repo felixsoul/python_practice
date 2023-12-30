d8cf059f8f9c591837d7e36e86dbf1cd00b6c839
hunter <sunnyhour@hotmail.com>
2023-12-30 09:21:36 +0800

    modified:   selenium_practice/test_baidu.py

--- selenium_practice/test_baidu.py
+++ selenium_practice/test_baidu.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
-driver_path = "/Users/hunter/local/chromedriver-mac-x64/chromedriver"
+#driver_path = "/Users/hunter/local/chromedriver-mac-x64/chromedriver"
+#driver = webdriver.Chrome(driver_path)
+driver_path = "D:\\local\\chromedriver-win64\\chromedriver.exe"
 driver = webdriver.Chrome(driver_path)
 driver.get("https://www.baidu.com")
 driver.find_element_by_id("kw").send_keys("Selenium")
