24bca1108a4aa382c2335ac20d99fda026f61e02
hunter <sunnyhour@hotmail.com>
2024-01-02 11:27:00 +0800

    new file:   selenium_practice/__pycache__/global_variables.cpython-311.pyc new file:   selenium_practice/browser_win.py modified:   selenium_practice/global_variables.py

--- selenium_practice/global_variables.py
+++ selenium_practice/global_variables.py
@@ -1,2 +1,3 @@
 driver_path = "D:\\local\\chromedriver-win64\\chromedriver.exe"
+driver_path_office = "C:\\work\\learn\\chromedriver-win64\\chromedriver.exe"
 driver_path_mac = "/Users/hunter/local/chromedriver-mac-x64/chromedriver"
