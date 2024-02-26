936a54db1f041384ad5ef6204e2e5f6ff069c235
hunter <sunnyhour@hotmail.com>
2024-02-26 14:04:37 +0800

    modified:   selenium_practice/unittest_examples/run_tests.py

--- selenium_practice/unittest_examples/run_tests.py
+++ selenium_practice/unittest_examples/run_tests.py
@@ -1,8 +1,15 @@
 import unittest
-
+import htmltestreport
+from test_case.test_baidu import TestBaidu
 test_dir = './test_case'
 suite = unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
 
 if __name__ == '__main__':
-    runnner = unittest.TextTestRunner()
-    runnner.run(suite)
+    fp = open('./test_report/result.html','wb')
+    # runnner = unittest.TextTestRunner()
+    # runnner.run(suite)
+    suite = unittest.TestSuite()
+    suite.addTest(TestBaidu("test_search_key_selenium"))
+    runner = htmltestreport.HTMLTestReport('./test_report/result.html',title="Test Baidu", description="测试百度" )
+    #runner = htmltestreport.HTMLTestReport(fp,title="Test Baidu", description="测试百度")
+    runner.run(suite)
