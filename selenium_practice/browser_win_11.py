e7bad9f282825abf5f3b450fb24379ef6a443764
hunter <sunnyhour@hotmail.com>
2024-03-11 09:42:47 +0800

    modified:   selenium_practice/browser_win_10.py

--- selenium_practice/browser_win_11.py
+++ selenium_practice/browser_win_11.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 from selenium import webdriver
 from global_variables import driver_path_office
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,15 +10,7 @@ print(main_window)
 above = browser.find_element_by_xpath('//*[@id="s-usersetting-top"]')
 ActionChains(browser).move_to_element(above).perform()
 option_to_select = browser.find_element_by_xpath('//*[@id="s-user-setting-menu"]/div/a[1]').click()
-all_handles = browser.window_handles
-sleep(2)
-for handle in all_handles:
-    print(handle)
-    
-browser.find_element_by_xpath('//*[@id="se-setting-7"]/a[2]').click()
-sleep(2)
-alert = browser.switch_to.alert
-alert_text = alert.text
-print(alert_text)                   
-alert.accept()
-browser.quit()
+
+sleep(5)
+browser.find_element_by_css_selector('li.item.cur[data-tabid="advanced"][href="#advanced"]').click()
+#browser.find_element_by_xpath('//*[@id="wrapper"]/div[6]/div/div/ul/li[2]').click()
