9cfc58a35df5a70c62f77d44a446cd22b175fd78
Hunter <sunnyhour@hotmail.com>
2024-06-08 12:35:51 +0800

    modified:   ml/first/test2.py

--- ml/first/test2.py
+++ ml/first/test2.py
@@ -2,3 +2,22 @@ x = 88
 y = -8.8
 z = 8 + 8j
 print(type(x), type(y), type(z))
+
+print(int('8'))
+print(int(8.88))
+print(int(True))
+
+x = 88.8
+y = 8
+
+x_to_int = int(x)
+print(x_to_int)
+
+y_to_float = float(y)
+print(y_to_float)
+
+y_to_complex = complex(y)
+print(y_to_complex)
+
+x_to_str = str(x)
+print(x_to_str)
