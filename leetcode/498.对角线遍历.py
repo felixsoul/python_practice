b15fd3de10e9737e9a55731eca2793d371ee3cb7
hunter <sunnyhour@hotmail.com>
2025-01-17 17:21:12 +0800

    new file:   "leetcode/498.\345\257\271\350\247\222\347\272\277\351\201\215\345\216\206.py"

---
+++ leetcode/498.对角线遍历.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=498 lang=python
+#
+# [498] 对角线遍历
+#
+
+# @lc code=start
+class Solution(object):
+    def findDiagonalOrder(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[int]
+        """
+        m = len(mat)
+        n = len(mat[0])
+        ans = []
+        for i in range(m+n-1):
+            print(i)
+            if i % 2:
+                x = 0 if i < n else i-n+1
+                y = i if i < n else n - 1
+                while x < m and y >= 0:
+                    ans.append(mat[x][y])
+                    x += 1
+                    y -= 1
+            else:
+                x = i if i < m else m - 1
+                y = 0 if i < m else i - m + 1
+                while x >= 0 and y < n:
+                    ans.append(mat[x][y])
+                    x -= 1
+                    y += 1
+
+        print(ans)
+        return ans
+
+
+test = Solution()
+print(test.findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+
+# @lc code=end
