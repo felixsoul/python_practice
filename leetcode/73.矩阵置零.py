783462d02f66b5d6798204fa0324afcf603106b7
hunter <sunnyhour@hotmail.com>
2025-01-21 16:27:09 +0800

    new file:   "leetcode/73.\347\237\251\351\230\265\347\275\256\351\233\266.py"

---
+++ leetcode/73.矩阵置零.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=73 lang=python
+#
+# [73] 矩阵置零
+#
+
+# @lc code=start
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
+# @lc code=end
+
