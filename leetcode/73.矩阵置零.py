601046ce61e4ba67191a03964563d218748f85b4
hunter <sunnyhour@hotmail.com>
2025-01-22 11:32:28 +0800

    modified:   "leetcode/73.\347\237\251\351\230\265\347\275\256\351\233\266.py"

--- leetcode/73.矩阵置零.py
+++ leetcode/73.矩阵置零.py
@@ -11,6 +11,18 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        
-# @lc code=end
+        m = len(matrix)
+        n = len(matrix[0])
+        row, col = [False]*m, [False]*n
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    row[i] = col[j] = True
+        for i in range(m):
+            for j in range(n):
+                if row[i] or col[j]:
+                    matrix[i][j] = 0
+        return matrix
 
+
+# @lc code=end
