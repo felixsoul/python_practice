443d29b835e05c757799e1fc7ec136c159f69169
hunter <sunnyhour@hotmail.com>
2025-01-23 14:07:50 +0800

    new file:   "leetcode/289.\347\224\237\345\221\275\346\270\270\346\210\217.py"

---
+++ leetcode/289.生命游戏.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=289 lang=python
+#
+# [289] 生命游戏
+#
+
+# @lc code=start
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        
+# @lc code=end
+
