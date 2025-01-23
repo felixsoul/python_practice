7d68076688d2dfb78abdf1e0a5ba944030d5de24
hunter <sunnyhour@hotmail.com>
2025-01-23 14:32:46 +0800

    modified:   "leetcode/289.\347\224\237\345\221\275\346\270\270\346\210\217.py"

--- leetcode/289.生命游戏.py
+++ leetcode/289.生命游戏.py
@@ -11,6 +11,28 @@ class Solution(object):
         :type board: List[List[int]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        
-# @lc code=end
+        rows = len(board)
+        columns = len(board[0])
+        board_cp = [[board[row][column]
+                     for column in range(columns)] for row in range(rows)]
+        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1),
+                     (-1, 0), (-1, 1), (0, 1), (1, 1)]
+        for row in range(rows):
+            for column in range(columns):
+                liveNeighbors = 0
+                for neighbor in neighbors:
+                    r = row + neighbor[0]
+                    c = column + neighbor[1]
+                    if (0 <= r < rows and 0 <= c < columns and board_cp[r][c] == 1):
+                        liveNeighbors += 1
+                if liveNeighbors < 2:
+                    board[row][column] = 0
+                elif liveNeighbors == 2:
+                    board[row][column] = board_cp[row][column]
+                elif liveNeighbors == 3:
+                    board[row][column] = 1
+                else:
+                    board[row][column] = 0
 
+
+# @lc code=end
